3e883fd85a91e91aef68d2f05e5a3abad42626e0
Sagar Ganiga <2015sagar.ganiga@ves.ac.in>
2018-10-06 07:13:17 +0530

    Algorithm

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Auth;
 use App\internships;
+use App\internshipSkills;
+use App\Skills;
 class EventsController extends Controller
 {
     public function registerForEvent(Request $request){
@@ -17,14 +19,25 @@ class EventsController extends Controller
         // Get internship details of all internship done by participant
         $internship_details = "";
 
+        $internship_skills = array();
+
         $internships = internships::where("user_id", "=", $user_id)->get();
-        
-        // return $internships;
         
         foreach($internships as $intern){
             $internship_details = $internship_details." ".$intern->Description;
+            $internship_id = $intern->id;
+            $skills = internshipSkills::where("internship_id", "=", $internship_id)->get();
+            // return $skills;
+            foreach($skills  as $skill){
+                // return $skill;
+                $s = Skills::where("id", "=", $skill->skill_id)->get();
+                // $internship_skills.push($skill);
+                array_push($internship_skills, $s->name);
+            }
         }
-        return $internship_details;
+
+        return $internship_skills;
+        
 
 
     }
